eda33015a8249eec37aa34e5b1eba6dded2b4b57
Michael Schär <mdschaer@gmail.com>
2018-05-21 18:42:24 +0200

    Mesh bug fix

--- pyramide2.py
+++ pyramide2.py
@@ -2,49 +2,48 @@ import bpy
 import bmesh
 import mathutils
 
-# === Definitions ===
+# === Setup ===
 
-def CubeName():
-    return "Stone"  
+CubeName = "Stone"
+CubeSize = 1
+CubeHalf = CubeSize / 2
+PyramideDepth = 7
+scn = bpy.context.scene
 
-def CubeSize():
-    return 1  
-
-def CubeHalf():
-    return CubeSize() / 2   
-
-def PyramideDepth():
-    return 2
- 
 
 # === Logic ===
 
 def create_pyramide():
     create_cube()
     create_side()
-    '''join_objects(True)
+    join_objects(True)
     duplicate_side()
     join_objects(False)
-    apply_modifiers()'''
+    apply_modifiers()
     
     
 def create_side():
-    Stone = bpy.data.objects[CubeName()]
-  
-    for z in range(1, PyramideDepth()):
+    Stone = bpy.data.objects[CubeName]
+      
+    for z in range(1, PyramideDepth):
         for y in range(0, z): 
             print(z,y)
             Stone.select = True
-            Stone.location = mathutils.Vector((CubeHalf()*z, -CubeHalf()*z+y, -z))
-            bpy.ops.object.duplicate()
-            bpy.ops.object.select_all(action='DESELECT')
+            Stone.location = mathutils.Vector((CubeHalf*z, -CubeHalf*z+y*CubeSize, -z*CubeSize))
+            duplicate_selected(Stone)
+            
 
-            #bpy.ops.object.select = False #TODO deselect not working
-
+def duplicate_selected(Stone):
+    new_obj = Stone.copy()
+    new_obj.data = Stone.data.copy()
+    new_obj.animation_data_clear()
+    scn.objects.link(new_obj)
+    bpy.ops.object.select_all(action='DESELECT')
+    
     
 def duplicate_side():
     print ("dupli")   
-    Stone = bpy.data.objects[CubeName()]
+    Stone = bpy.data.objects[CubeName]
     Stone.select = True
     
     for x in range(0, 3):
@@ -55,7 +54,7 @@ def duplicate_side():
 
 def join_objects(ignoreTopStone):
     for ob in bpy.context.scene.objects:
-        if ob.type == 'MESH' and (not ignoreTopStone or ob.name != CubeName() + '.001'):
+        if ob.type == 'MESH' and (not ignoreTopStone or ob.name != CubeName + '.001'):
             ob.select = True
             bpy.context.scene.objects.active = ob
         else:
@@ -66,11 +65,11 @@ def join_objects(ignoreTopStone):
     
 def create_cube():
     bpy.ops.mesh.primitive_cube_add(
-        radius=CubeHalf(), 
+        radius=CubeHalf, 
         location=(0, 0, 0)
     )
     NewObject = bpy.context.active_object
-    NewObject.name = CubeName()
+    NewObject.name = CubeName
     bpy.ops.object.duplicate()  
 
 
